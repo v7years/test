66a1f1174ad199b762191f46e6a47b17d71655cc
tanwei <wei.tan3@geely.com>
2021-02-05 11:06:10 +0800

    init

---
+++ library/src/main/java/com/gleely/library/TestClass.java
@@ -0,0 +1,14 @@
+package com.gleely.library;
+
+/**
+ * Description
+ * Author: wei.tan3
+ * Created Time: 2021/2/5
+ */
+public class TestClass {
+
+    public void test(){
+        System.out.printf("这是一个lib");
+    }
+
+}
